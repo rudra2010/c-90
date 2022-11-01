d48ab82444c6576e3041134fc1caf9db60e9d3cb
rudra2010 <102788949+rudra2010@users.noreply.github.com>
2022-11-01 15:57:52 +0530

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -1,7 +1,7 @@
 function add_user(){
-    player1_name=document.getElementById("player1_name_input").value;
-    player2_name=document.getElementById("player2_name_input").value;
-localStorage.setItem("player1_name",player1_name);
-localStorage.setItem("player2_name",player2_name);
-window.location="quiz_game_login.html";
+    player_1_name=document.getElementById("player1_name_input").value;
+    player_2_name=document.getElementById("player2_name_input").value;
+    localStorage.setItem("player_1_name",player_1_name);
+    localStorage.setItem("player_2_name",player_2_name);
+    window.location="quiz_game_login.html";
 }
--- quiz_game_login.js
+++ quiz_game_login.js
@@ -1,6 +1,25 @@
-function send(){
-    number1=document.getElementById("number1").value;
-    number2=document.getElementById("number2").value;
-    actual_answer=parseInt(number1)*(number2);
-
+player_1_name=localStorage.getItem("player_1_name");
+player_2_name=localStorage.getItem("player_2_name");
+player_1_score=0;
+player_2_score=0;
+document.getElementById("player_1_name").innerHTML=player_1_name;
+document.getElementById("player_2_name").innerHTML=player_2_name;
+document.getElementById("player_1_score").innerHTML=player_1_score;
+document.getElementById("player_2_score").innerHTML=player_2_score;
+document.getElementById("player_question").innerHTML="Question Turn "+player_1_name;
+document.getElementById("player_answer").innerHTML="Answer Turn "+player_2_name;
+function send1(){
+    number1=document.getElementById("1").value;
+    number2=document.getElementById("2").value;
+    actual_answer=parseInt(number1)* parseInt(number2);
+    question_number="<h4>"+number1+"X"+ number2+"</h4>";
+    input_box="<br>Answer: <input type='text' id='input_check_box'>";
+    check_button="<br><br><button class='btn-btn-info' onclick='check()'>CHECK</button>";
+    row=question_number+input_box+check_button;
+    document.getElementById("output").innerHTML=row;
+    document.getElementById("1").value="";
+    document.getElementById("2").value="";
+    
 }
+question_turn="player1";
+answer_turn="player2";
